7ed5024d1d56145141af336085057146a8b1bbc2
Shiyas <shiyas.cholamukhath@tabcorp.com.au>
2019-03-14 19:02:10 +1100

    Revert "enable newrelic"
    This reverts commit 674898c42a41443e48389a108d3efe45cb12e4eb.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const newrelic = require('newrelic');
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
